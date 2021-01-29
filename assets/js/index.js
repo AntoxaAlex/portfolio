fe9b37e8c0f5787a0fc8523927345962899296d2
AntoxaAlex <50088266+AntoxaAlex@users.noreply.github.com>
2021-01-29 17:57:55 +0200

    Portfolio v1

---
+++ assets/js/index.js
@@ -0,0 +1,113 @@
+let mainOffset = $("#main-div .row").offset();
+let aboutOffset = $("#about-div").offset();
+let skillsOffset = $("#skills-div #skills-row").offset();
+let portfolioOffset = $("#portfolio-div #portfolio-row").offset();
+let contactOffset = $("#contact-row").offset();
+
+$("#main_links").on("mouseenter mouseleave", "li",function(){
+    $(this).toggleClass("showParagraph");
+})
+$(".folio-item").on("mouseenter mouseleave",function(){
+    $(this).toggleClass("showPanel",1000);
+})
+
+$(".show-preview").on("click",function () {
+    const id = $(this).attr("id")
+    let link = "";
+    switch (id) {
+        case "y-trip":
+            link = "https://your-trip.herokuapp.com/";
+            break;
+        case "cyber-chat":
+            link = "https://cyberchat95.herokuapp.com/"
+            break;
+        default:
+            alert("No such project")
+    }
+    if(link !== ""){
+        $("iframe").attr("src",link);
+        $("#modal-div").removeClass("modal-body").addClass("show-modal-div");
+    }
+})
+
+$("#close-iframe-btn").on("click",function () {
+    $("#modal-div").removeClass("show-modal-div").addClass("modal-body")
+})
+
+$(".navbar-brand").on("click", function () {
+    $('html, body').animate({
+        scrollTop: mainOffset.top,
+        scrollLeft: mainOffset.left
+    });
+})
+$("#about-btn").on("click", function () {
+    $('html, body').animate({
+        scrollTop: aboutOffset.top,
+        scrollLeft: aboutOffset.left
+    });
+})
+$("#skills-btn").on("click", function () {
+    $('html, body').animate({
+        scrollTop: skillsOffset.top,
+        scrollLeft: skillsOffset.left
+    });
+})
+$("#folio-btn").on("click", function () {
+    $('html, body').animate({
+        scrollTop: portfolioOffset.top,
+        scrollLeft: portfolioOffset.left
+    });
+})
+$("#contact-btn").on("click", function () {
+    $('html, body').animate({
+        scrollTop: contactOffset.top,
+        scrollLeft: contactOffset.left
+    });
+})
+
+
+//Typing and fade animation
+$(document).ready(function () {
+    console.log("Document is ready")
+    //Array with text to type
+    let textArr = ["Hi,","I am Anton,","full-stack web developer"]
+    // Type one text in the typwriter
+    // Keeps calling itself until the text is finished
+    function typeWriter(text,headIndex,i,fnCallback) {
+        // Check if text isn't finished yet
+        if(i<text.length){
+            //Add next char
+            $(`#header${headIndex}`).html(text.substring(0,i+1) + '<span aria-hidden=\"true\"></span>')
+            // wait for a while and call this function again for next char
+            setTimeout(function () {
+                typeWriter(text,headIndex,i+1,fnCallback);
+            },100);
+        }
+        // text finished, call callback if there is a callback function
+        else if(typeof fnCallback === "function"){
+            setTimeout(fnCallback, 600);
+        }
+    }
+    //  Start typing animation for text in textArr
+    function startTextAnimation(i, headIndex) {
+        if (typeof textArr[i] == 'undefined'){
+            $("#main-div .headers-div").append('<button class="btn btn-lg btn-class mt-3">See more</button>')
+            $("#main-div button").on("click", function () {
+                $('html, body').animate({
+                    scrollTop: aboutOffset.top,
+                    scrollLeft: aboutOffset.left
+                });
+            })
+            $("#main-div img").fadeTo("slow",1)
+        }
+        //  Check if textArr[i] exists
+        if(i < textArr.length){
+            typeWriter(textArr[i],headIndex,0,function () {
+                $(`#header${headIndex} span`).remove();
+                startTextAnimation(i+1, headIndex + 1);
+            });
+        }
+    }
+    startTextAnimation(0,1)
+})
+
---
+++ assets/js/map.js
@@ -0,0 +1,155 @@
+const myStyle = [
+    {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 13
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#144b53"
+            },
+            {
+                "lightness": 14
+            },
+            {
+                "weight": 1.4
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#08304b"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#0c4152"
+            },
+            {
+                "lightness": 5
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#0b434f"
+            },
+            {
+                "lightness": 25
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#0b3d51"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#146474"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#021019"
+            }
+        ]
+    }
+]
+
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 46.2953539495982, lng: 30.6526900891774 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 14,
+        center: uluru,
+        disableDefaultUI: true,
+        styles: myStyle
+    });
+    // The marker, positioned at Uluru
+    new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+}
